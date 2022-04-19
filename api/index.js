2b6d76c8f298adb31394f43308773c6c6cb9faeb
LucianaCHA <76781967+LucianaCHA@users.noreply.github.com>
2022-04-19 01:35:40 -0300

    nueva actualización

--- api/index.js
+++ api/index.js
@@ -17,19 +17,12 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
-let port = process.env.PORT;
-if (port === undefined || port === "") {
-  port = 3001;
-}
-
-
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(port , () => {
-    console.log('%s listening at ', port); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
