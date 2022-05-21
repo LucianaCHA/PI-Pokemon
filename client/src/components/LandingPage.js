40e574c5de5ac3b75ad6ebc8c7ecc7b166f6dfb6
LucianaCHA <lucianachamorro87@gmail.com>
2022-05-20 21:23:06 -0300

    landing page image updated :)

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -7,6 +7,8 @@ import { useDispatch } from "react-redux";
 
 import { getAllData } from "../actions";
 
+import imagen from "../assets/bkg2.jpg"; 
+
 import styles from "./LandingPage.module.css";
 
 export default function LandingPage() {
@@ -18,8 +20,15 @@ export default function LandingPage() {
 
   return (
     <React.Fragment>
+    <div className={styles.container}>
     <div className={styles.bkg}>
+    <div className={styles.landingImg}>
+      <img src={imagen} style= {{
+        position:'absolute', filter: 'blur(15px)', zIndex: '-10', margin: '-3% -50%', width: '100vw', height: '100vh', objectFit: 'cover'
+      }} alt="imagenpokemon" />
       <Link to="/home"><button className = {styles.go}>GO!</button></Link>
+      </div>
+      </div>
       </div>
     </React.Fragment>
   );
