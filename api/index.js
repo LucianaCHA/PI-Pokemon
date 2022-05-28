d696d462391d2f13160151ade295b4ace95bdd78
LucianaCHA <lucianachamorro87@gmail.com>
2022-05-28 18:37:53 -0300

    index updated

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+
 // const PORT = PORT ||3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
+    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
