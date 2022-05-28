912608b849372f0481b19a8c66a94e7879a7c2c3
LucianaCHA <lucianachamorro87@gmail.com>
2022-05-28 16:23:15 -0300

    make it better

--- api/index.js
+++ api/index.js
@@ -21,11 +21,11 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-var PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 // const PORT = PORT ||3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log("port", PORT); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log("port", port); // eslint-disable-line no-console
   });
 });
